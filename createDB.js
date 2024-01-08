d8f9694084940ed7422795a8b6a07b735cc5c24d
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-08 17:57:15 +0300

    Сделала посев данных

--- createDB.js
+++ createDB.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient
 var data = require("./data.js").data
-const uri = "mongodb://localhost:27017/"
+
+const uri = "mongodb://127.0.0.1:27017/"
 const client = new MongoClient(uri)
 async function run() {
 try {
@@ -9,8 +10,8 @@ var database = client.db("funfiction");
 database.dropDatabase()
 database = client.db("funfiction");
 const funf = database.collection("funf");
-const result = await funf.insertOne({name:"Кефер"});
-console.log(`${result} documents were inserted`);
+const result = await funf.insertMany(data);
+console.log(`${result.insertedCount} documents were inserted`);
 } finally {
 await client.close();
 }
