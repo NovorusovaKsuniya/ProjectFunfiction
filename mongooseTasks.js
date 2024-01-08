178a3b3e85f1b90d16de8e4a9950d42a6cbd1c11
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-08 18:37:17 +0300

    Создала и подключила схему

--- mongooseTasks.js
+++ mongooseTasks.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://127.0.0.1:27017/Replika');
 
-const Fanf = mongoose.model('Fanf', { name: String });
+var schema = mongoose.Schema({name:String});
+schema.methods.talk = function(){
+    console.log(this.get("name") + " сказал приветик")
+    }
+const Fanf = mongoose.model('Fanf', schema);
 const fanf = new Fanf({ name: 'Кефер' });
 
-fanf.save().then(() =>  console.log('Приветики :)'))
+fanf.save().then(() => fanf.talk())
