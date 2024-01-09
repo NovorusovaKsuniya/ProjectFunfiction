5901d9ce54daf633918b30bfbfc959954f0ccc0c
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-09 18:09:37 +0300

    Подготовила данные для их передачи на сервер

--- routes/index.js
+++ routes/index.js
@@ -15,5 +15,10 @@ router.get('/', async (req, res, next) => {
 router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Вход'});
 });
-  
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+
+});
+
 module.exports = router;
