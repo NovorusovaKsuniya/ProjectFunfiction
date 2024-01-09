d72386af6f2be83af4c0c91f9afa14e7dc059f3c
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-09 18:14:33 +0300

    Прописала логику пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Char = require("../models/character").Char;
+var User = require("./../models/user").User
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -15,10 +16,27 @@ router.get('/', async (req, res, next) => {
 router.get('/logreg', function(req, res, next) {
     res.render('logreg',{title: 'Вход'});
 });
+
 router.post('/logreg', function(req, res, next) {
-    var username = req.body.username
-    var password = req.body.password
-
+  var username = req.body.username
+  var password = req.body.password
+  User.findOne({username:username},function(err,user){
+      if(err) return next(err)
+        if(user){
+          if(user.checkPassword(password)){
+          req.session.user = user._id
+          res.redirect('/')
+        } else {
+          res.render('logreg', {title: 'Вход'})
+      }
+    } else {
+      var user = new User({username:username,password:password})
+      user.save(function(err,user){
+        if(err) return next(err)
+        req.session.user = user._id
+        res.redirect('/')
+      })
+    }
+  })
 });
-
 module.exports = router;
