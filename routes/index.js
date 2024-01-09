4f24ee3c1c3292c13cbc3dd4380a4bb300d551fb
Ксюня Новорусова <145188437+NovorusovaKsuniya@users.noreply.github.com>
2024-01-09 16:20:34 +0300

    Некоторые правки

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
-
+const Char = require("../models/tree").Char;
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-    const menu = await Tree.find({}, { _id: 0, title: 1, nick: 1 });
+    const menu = await Char.find({}, { _id: 0, title: 1, nick: 1 });
     req.session.greeting = "Hi!!!"
     res.render('index', { title: 'Express', menu:menu });
   } catch (err) {
