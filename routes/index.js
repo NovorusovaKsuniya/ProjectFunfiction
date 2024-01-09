5214da350afedc476ff46a5109f4d3f3de1be345
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-09 20:22:22 +0300

    Создала таблицу User и прописала логику пользователя

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
 const Char = require("../models/character").Char;
-var User = require("./../models/user").User
-var checkAuth = require("./../middleware/checkAuth.js")
+//var User = require("./../models/user").User
+var db = require('../mySQLConnect.js');
+
 
 /* GET home page. */
 router.get('/', async (req, res, next) => {
@@ -19,27 +20,28 @@ router.get('/logreg', async function(req, res, next) {
   res.render('logreg', { title: 'Вход',error:null}); 
 });
 
-router.post('/logreg', async function(req, res, next) {
-  const username = req.body.username;
-  const password = req.body.password;
-  try {
-      const user = await User.findOne({ username });
-      if (user) {
-          if (await user.checkPassword(password)) {
-              req.session.user = user._id;
-              res.redirect('/');
-          } else {
-              res.render('logreg', { title: 'Вход', error: 'Неверный пароль' });
-          }
+router.post('/logreg', function(req, res, next) {
+    var username = req.body.username
+    var password = req.body.password
+    db.query (`SELECT * FROM user WHERE user.username = '${req.body.username}'`,
+    function(err,users){
+      if(err) return next(err)
+      if(users.length > 0) {
+          var user = users[0];
+        if (password == user.password){
+          req.session.user = user.id
+          res.redirect('/')
+        } else {
+          res.render('logreg', {title: 'Вход'})
+        }
       } else {
-          const newUser = new User({ username, password });
-          await newUser.save();
-          req.session.user = newUser._id;
-          res.redirect('/');
-      }
-  } catch (err) {
-      next(err);
+        db.query(`INSERT INTO user (username, password) VALUES ('${username}','${password}')`, function(err, user){
+      if(err) return next(err)
+      req.session.user = user.id
+      res.redirect('/')
+    })
   }
+  })
 });
 
 router.post('/logout', function(req, res, next) {
