6109e92adad1bf865eb0c9635fb1fcb8b9e2e6c0
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-09 17:24:31 +0300

    Сделала новую модель для пользователя

---
+++ checkUser.js
@@ -0,0 +1,13 @@
+var mongoose = require("mongoose")
+mongoose.connect("mongodb://127.0.0.1/all")
+var User = require("./models/user.js").User
+var first_user = new User({
+username: "Vasya",
+password: "qwerty"
+})
+
+first_user.save().then((user) => {
+    console.log(user);
+  }).catch((err) => {
+    throw err;
+  });
