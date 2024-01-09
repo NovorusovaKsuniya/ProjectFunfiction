cbb2e588726a4148f731a57423ffd5e6d8ba3bfc
NovorusovaKsuniya <Ks21072004@yandex.ru>
2024-01-09 15:46:01 +0300

    Установила модуль express-session и настроила его

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/funfiction')
+var session = require("express-session")
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -17,6 +18,13 @@ app.engine('ejs',require('ejs-locals'));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(session({
+  secret: "Fanfic",
+  cookie:{maxAge:60*1000},
+  resave: true,
+  saveUninitialized: true
+  }))
+  
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
