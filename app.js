8fe806df60f6018f916e1281368d5bb69374beee
Ксюня Новорусова <145188437+NovorusovaKsuniya@users.noreply.github.com>
2024-01-10 14:10:11 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,6 @@ var logger = require('morgan');
 var bodyParser = require('body-parser');
 //var mongoose = require('mongoose');
 var session = require("express-session")
-//var Tree = require("./models/tree").Tree
-//mongoose.connect('mongodb://127.0.0.1/forest');
 var mysql2 = require('mysql2/promise');
 var MySQLStore = require('express-mysql-session')(session);
 
@@ -53,14 +51,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// var MongoStore = require('connect-mongo');
-// app.use(session({
-//   secret: "Forest",
-//   cookie:{maxAge:60*1000},
-//   resave: true,
-//   saveUninitialized: true,
-//   store: MongoStore.create({mongoUrl: 'mongodb://localhost/forest'})
-//   }))
 
 
   app.use(function(req,res,next){
