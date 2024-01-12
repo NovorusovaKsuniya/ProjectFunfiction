764360bb8a400830ec362a06ba19a921b49dee74
Ксюня Новорусова <145188437+NovorusovaKsuniya@users.noreply.github.com>
2024-01-13 00:18:57 +0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async (req, res, next) => {
   try {
-    const menu = await Tree.find({}, { _id: 0, title: 1, nick: 1 });
+    const menu = await Char.find({}, { _id: 0, title: 1, nick: 1 });
     res.cookie('greeting','Hi!!').res.render('index', {
       title: 'Express',
       menu: menu
